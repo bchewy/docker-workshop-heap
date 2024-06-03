a4b5347d343f17dbbaff76a50304b72737bc2b00
franky-lim24 <franky.2021@scis.smu.edu.sg>
2024-06-03 20:11:15 +0800

    add cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 from app import crud, models, schemas
 from app.database import SessionLocal, engine
@@ -7,10 +8,15 @@ from app.database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Dependency
-
-
 def get_db():
     db = SessionLocal()
     try:
@@ -18,20 +24,14 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/")
-def health_check():
-    return "I'm health!"
-
 @app.post("/books/", response_model=schemas.Book)
 def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
     return crud.create_book(db=db, book=book)
-
 
 @app.get("/books/", response_model=List[schemas.Book])
 def read_books(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     books = crud.get_books(db, skip=skip, limit=limit)
     return books
-
 
 @app.get("/books/{book_id}", response_model=schemas.Book)
 def read_book(book_id: int, db: Session = Depends(get_db)):
@@ -40,11 +40,9 @@ def read_book(book_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Book not found")
     return db_book
 
-
 @app.put("/books/{book_id}", response_model=schemas.Book)
 def update_book(book_id: int, book: schemas.BookCreate, db: Session = Depends(get_db)):
     return crud.update_book(db=db, book_id=book_id, book=book)
-
 
 @app.delete("/books/{book_id}", response_model=schemas.Book)
 def delete_book(book_id: int, db: Session = Depends(get_db)):

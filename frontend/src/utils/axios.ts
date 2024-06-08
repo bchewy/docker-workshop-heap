79b90000cc14b30c1ac5e031a34910ca17a0943e
Brian Chew <brian.chew.2022@scis.smu.edu.sg>
2024-06-08 22:53:13 +0800

    it works now

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: 'http://0.0.0.0:8080',
 });
+
+// instance.interceptors.request.use(request => {
+//   console.log('Axios instance is being used with request:', request);
+//   return request;
+// });
 
 export default instance;
